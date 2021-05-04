63fc2854f46c56a61582816bae0eb0727919ff88
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-04 21:41:57 +0200

    + Added service worker + Included dev client in cors origins + Added minification of css

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,12 @@ app.use(express.static('dist'));
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -14,20 +18,27 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
+    },
+    optimization: {
+        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
