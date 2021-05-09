3c41ce2262b81da473c454863b4c6d02bf598bea
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-09 11:54:10 +0200

    Exclude meta files from test
    + Added an exclusion of __MACOS folder to jest config
    + Added an exclusion of __MACOS folder to webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -20,7 +20,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: /node_modules|__MACOS/,
                 loader: "babel-loader"
             },
             {
--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: /node_modules|__MACOS/,
                 loader: "babel-loader"
             },
             {
