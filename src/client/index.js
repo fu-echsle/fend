5374c041cc8f0f90cb188968ed2bbfe1be698fed
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-24 19:12:04 +0200

    Visibility changes for the initial page load

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,44 @@ import './media/icon.ico';
 import './media/icon-32.png';
 import './media/icon-96.png';
 import './media/icon-180.png';
+import './media/like.svg';
+import './media/comment.svg';
+import './media/expand.svg';
+import './media/collapse.svg';
+import {toggleVisibility} from './js/helpers';
+import {showPreviousSearches} from './js/showPreviousSearches';
+
+const initialize = () => {
+    const sectionStart = document.querySelector('#start');
+    const sectionDestinations = document.querySelector('#destinations');
+    const sectionImages = document.querySelector('#images');
+    const sectionForecast = document.querySelector('#forecast');
+    const sectionPrevious = document.querySelector('#previous');
+    sectionStart.querySelector('h2:first-of-type').addEventListener('click', () => {
+        toggleVisibility(sectionStart);
+    });
+    toggleVisibility(sectionStart, true);
+
+    sectionDestinations.querySelector('h2:first-of-type').addEventListener('click', () => {
+        toggleVisibility(sectionDestinations);
+    });
+
+    sectionImages.querySelector('h2:first-of-type').addEventListener('click', () => {
+        toggleVisibility(sectionImages);
+    });
+
+    sectionForecast.querySelector('h2:first-of-type').addEventListener('click', () => {
+        toggleVisibility(sectionForecast);
+    });
+
+    sectionPrevious.querySelector('h2:first-of-type').addEventListener('click', () => {
+        toggleVisibility(sectionPrevious);
+    });
+
+    showPreviousSearches();
+};
+
+initialize();
 
 export {
     pickDestionation,
