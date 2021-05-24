4b9057ba580f212117aeb7a7979a2898bc7fc0a7
Yvonne Vogt <fu-echsle@siebenelf.com>
2021-05-24 19:11:28 +0200

    Added functionality to list previous searches

---
+++ src/client/js/showPreviousSearches.js
@@ -0,0 +1,55 @@
+import {getFromLocalStorage} from './helpers';
+
+function showPreviousSearches() {
+    const resultContainer = document.getElementById('previousList');
+    const spinnerContainer = document.getElementById('spinner');
+
+    spinnerContainer.style.display = 'inherit';
+
+    const res = getFromLocalStorage();
+    if (res.error) {
+        const fragment = document.createDocumentFragment();
+        const resultWrapper = document.createElement('div');
+        const headline = document.createElement('h3');
+        headline.textContent = res.error;
+        resultWrapper.appendChild(headline);
+        resultWrapper.className = 'previousWrapper';
+        fragment.appendChild(resultWrapper);
+
+        resultContainer.replaceChild(fragment, document.querySelector('.previousWrapper'));
+    } else {
+        resultContainer.replaceChild(renderPreviousSearches(res), document.querySelector('.previousWrapper'));
+    }
+    spinnerContainer.style.display = 'none';
+}
+
+const renderPreviousSearches = (res) => {
+    const fragment = document.createDocumentFragment();
+    const resultWrapper = document.createElement('div');
+
+    res.forEach(elem => {
+        const newElem = document.createElement('div');
+        const city = document.createElement('h3');
+        const country = document.createElement('p');
+        const dateStart = document.createElement('p');
+        const dateEnd = document.createElement('p');
+
+        city.textContent = elem.city;
+        country.textContent = elem.country;
+        dateStart.textContent = elem.dateStart;
+        dateEnd.textContent = elem.dateEnd;
+
+        newElem.appendChild(city);
+        newElem.appendChild(country);
+        newElem.appendChild(dateStart);
+        newElem.appendChild(dateEnd);
+
+        resultWrapper.appendChild(newElem);
+    });
+
+    resultWrapper.className = 'previousWrapper';
+    fragment.appendChild(resultWrapper);
+    return fragment;
+};
+
+export {showPreviousSearches};
